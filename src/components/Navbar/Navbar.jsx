2b724522b696a1ddffdafb3509556a4662c3ae19
y-emam <YasserEmam2002@gmail.com>
2025-01-16 21:15:08 +0200

    making <a> inside <li> to improve accessability

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,15 +9,15 @@ function Navbar() {
           <img src={logo} alt="logo" className="logo" />
         </a>
         <ul className="nav-links">
-          <a href="/">
-            <li>Events</li>
-          </a>
-          <a href="/">
-            <li>Invites</li>
-          </a>
-          <a href="/">
-            <li>About</li>
-          </a>
+          <li>
+            <a href="/">Events</a>
+          </li>
+          <li>
+            <a href="/">Invites</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
         </ul>
       </nav>
       <hr />
