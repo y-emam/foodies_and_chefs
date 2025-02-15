911c00f620b05ddb5683b0c02b7ab0a27e74f092
y-emam <yasseremam2002@gmail.com>
2025-02-15 19:32:27 +0200

    Added some edits in profile page

--- src/services/profile/editProfile.js
+++ src/services/profile/editProfile.js
@@ -1,6 +1,16 @@
 const editProfileService = async (userData) => {
     try {
-        const res = await fetch('http://localhost:5000/api/users/editProfile', {});
+        const res = await fetch('http://localhost:5000/api/users/editProfile', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(userData)
+        });
+
+        const data = await res.json();
+
+        return data
     } catch (error) {
         console.log(error);
     }
