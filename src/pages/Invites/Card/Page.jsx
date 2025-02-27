a143e59ac54a1033699a4b4bf78b41fdc82493d5
y-emam <yasseremam2002@gmail.com>
2025-02-27 22:21:34 +0200

    Added more edits to the invitation card

--- src/pages/Invites/Card/Page.jsx
+++ src/pages/Invites/Card/Page.jsx
@@ -8,7 +8,6 @@ import ProfileSVG from "../../../assets/svg/Profile/Component";
 import TimeSVG from "../../../assets/svg/Time/Component";
 import LocationMap from "../../../components/LocationMap/Component";
 import DishImg from "../../../assets/images/img3.webp";
-import InvCardBgImg from "../../../assets/images/invCardBG1.png";
 import LogoImg from "../../../assets/images/logoWithoutText.png";
 import Spoon1Img from "../../../assets/images/spoon1.png";
 import Spoon2Img from "../../../assets/images/spoon2.png";
@@ -30,7 +29,7 @@ function InvitationCardPage() {
           <h1 className="font-ledger font-normal text-center md:text-[40px] text-lg text-[#F1F1F1] p-5 pb-5">
             The Greatest Event
           </h1>
-          <p className="mb-5 md:mb-10 plus-jakarta-sans text-center text-[#FA8836] md:text-[22px] text-sm italic font-normal">
+          <p className="mb-5 mx-8 md:mb-10 plus-jakarta-sans text-center text-[#FA8836] md:text-[22px] text-sm italic font-normal">
             Get ready for a day of fun, laughter, and unforgettable memories as
             we celebrate mohamed’s special day!
           </p>
@@ -63,18 +62,21 @@ function InvitationCardPage() {
                 <span> {`{ Start 8:00 } - { End 11:30 } `} </span>
               </div>
             </div>
-            <div className="flex items-center  text-[0.7rem] md:text-2xl">
+            <div className="flex items-center text-[0.7rem] md:text-2xl">
               <LocationSVG />
               <span>Zahraa ,Nasr city, Egypt</span>
             </div>
 
             <div className="flex relative w-full md:w-2/3 md:h-56 h-64 justify-center items-center">
-              {/* <LocationMap
-              location="Zahraa ,Nasr city, Egypt"
-              latitude="30.06263"
-              longitude="31.3467"
-            /> */}
+              <LocationMap
+                location="Zahraa ,Nasr city, Egypt"
+                latitude="30.06263"
+                longitude="31.3467"
+              />
             </div>
+
+            {/* Give some space under the map */}
+            <div className="h-10 " />
           </div>
           <div className="flex flex-col justify-center items-center w-full relative">
             <div className="inv-card-bg-img-1 before:absolute before:left-0 before:top-0 before:w-full before:h-full before:z-10 before:bg-[#00000080] flex flex-col justify-center items-center w-full npm bg-no-repeat min-h-60">
@@ -94,15 +96,25 @@ function InvitationCardPage() {
             >
               <input type="hidden" name="EventId" value="@Event.id" />
 
-              {/* Three spoons */}
-              <img
-                className="w-[48rem] h-auto absolute left-0 -z-10"
-                src={Spoon1Img}
-                alt=""
-              />
+              <div className="relative w-full flex flex-col items-center justify-center mb-20">
+                {/* Three spoons */}
+                <img
+                  className="w-[48rem] h-auto absolute -start-[12rem] top-0 -translate-x-10 -z-20"
+                  src={Spoon1Img}
+                  alt=""
+                />
+                <img
+                  className="w-[48rem] h-auto absolute -start-[4rem] top-[15rem] -translate-x-10 -z-20"
+                  src={Spoon2Img}
+                  alt=""
+                />
+                <img
+                  className="w-[48rem] h-auto absolute -start-[12rem] top-[30rem] -translate-x-10 -z-20"
+                  src={Spoon3Img}
+                  alt=""
+                />
 
-              <div className="w-full flex flex-col items-center justify-center">
-                <div className="max-h-fit flex flex-col npm  bg-no-repeat bg-auto w-full md:w-10/12 items-center">
+                <div className="mainbg-dark max-h-fit flex flex-col npm bg-no-repeat bg-auto w-11/12 md:w-9/12 items-center z-10 -translate-y-8">
                   {/* Chef name */}
                   <p className="font-ledger text-3xl text-[#FA8836] uppercase w-11/12 border-t-2 border-[#FA8836] pt-5 mt-14 text-center">
                     Chef: Yasser Emam
